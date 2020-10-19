771a28e99b18482f1163ba9ffbf888512699df41
uideodepot <71784595+uideodepot@users.noreply.github.com>
2020-10-20 01:47:42 +0800

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,76 @@
+// viewing ad random select
+function viewingAD() {
+var footSet = new Array();
+		footSet[1] = "https://file.al/1pjq8eh57tpu/3.m4v.html"; <!-- Nica & Sally - Lesbian Lovers (Threesome) -->
+		footSet[2] = "https://file.al/47d6hd28kpl9/2.m4v.html"; <!-- Nica -->
+		footSet[3] = "https://file.al/kr1phmhe4sfa/7.m4v.html"; <!-- Pinay Couple Banging While Parents Are Out -->
+		footSet[4] = "https://file.al/oujeuf733no8/4.m4v.html"; <!-- Party with Nica -->
+		footSet[5] = "https://file.al/fc9pzev0ksjk/Cartoon-from-Bangkok.wmv.html"; <!-- Cartoon from Bangkok Thailand Fucked in the Tub -->
+		footSet[6] = "https://file.al/x1p3b55s8kuh/Noot-from-Thailand.wmv.html"; <!-- Fucking Noot from Thailand -->
+		footSet[7] = "https://file.al/637sz9ztku2b/Agnes-%2526-Sunshine.wmv.html"; <!-- Hot Pinay Escorts Agnes & Sunshine -->
+		footSet[8] = "https://file.al/d2sw48rlujf0/Thai-Babe-Nok.wmv.html"; <!-- Hot Thai Babe Nok Moans with Pleasure -->
+		footSet[9] = "https://file.al/w294od5oygme/08.m4v.html"; <!-- Home -->
+		footSet[10] = "https://file.al/fh0etm1t093s/09.m4v.html"; <!-- Manila Exposed 4 -->
+		footSet[11] = "https://file.al/7wl2vnlfo75q/Pinay-Chicks-Janica-and-Janice.wmv.html"; <!-- Pinay Chicks Janica and Janice Kissing Each Other's Pussy -->
+		footSet[12] = "https://file.al/5d56xpqepyr5/Phon-from-Bangkok.wmv.html"; <!-- Phon from Bangkok Thailand Fucked in a Hotel -->
+		footSet[13] = "https://file.al/lvg64e7levm0/Thai-Girl-Not.wmv.html"; <!-- Thai Girl Not with Thai Pimp Danny Koh -->
+		footSet[14] = "https://file.al/qm2e4crbu2on/Mitch-Pinay.wmv.html"; <!-- Mitch Hot Pinay Amateur -->
+		footSet[15] = "https://file.al/nua1hxf5dqyo/Koreana-on-the-Beach.wmv.html"; <!-- Koreana Masturbating & Sucking on the Beach -->
+		footSet[16] = "https://file.al/x8di2cgtoiav/Donah-from-Indonesia.wmv.html"; <!-- Indonesian Bar Girl Donah Fucked POV -->
+		footSet[17] = "https://file.al/u6ljykakbph0/14.m4v.html"; <!-- Secret Toilet Camera Voyeur Japanese Student Caught Masturbating -->
+		footSet[18] = "https://file.al/jjacre4ogpah/12.m4v.html"; <!-- Rubbing My Pussy Till I Cum Hugely -->
+		footSet[19] = "https://file.al/t30pgaeu2jl7/8.m4v.html"; <!-- Japanese Girl Student's Room Sex Tutorial Voyeur Caught On Wednesday Jan 8th -->
+		footSet[20] = "https://file.al/ft4yhum7byqh/9-a.m4v.html"; <!-- Dorm Advantage -->
+		footSet[21] = "https://file.al/kwy2v9j8k841/9.m4v.html"; <!-- Pinay Fucked in Dorm -->
+		footSet[22] = "https://file.al/0z2kwqeuxswd/03.m4v.html"; <!-- Gottaluv Cam Show -->
+		footSet[23] = "https://file.al/1q4keuw154uw/10.m4v.html"; <!-- Real Lesbians Eating Pussy -->
+		footSet[24] = "https://file.al/wmu5x8fa0u7l/13.m4v.html"; <!-- Russian Amateur Swingers -->
+		footSet[25] = "https://file.al/pr9dvg9ogubq/15.m4v.html"; <!-- Security Hidden Camera Caught Sex Scandal -->
+		footSet[26] = "https://file.al/ncucgdvgaojv/6.m4v.html"; <!-- Asian Bound Made To Orgasm -->
+		footSet[27] = "https://file.al/o9sacdfa528q/16.m4v.html"; <!-- Shy Japanese College Girl Spied While Having Sex -->
+		footSet[28] = "https://file.al/descgtz6admo/17.m4v.html"; <!-- Sogo Motel Cubao Scandal -->
+		footSet[29] = "https://file.al/613yiiwwauvb/18.m4v.html"; <!-- Tianna Lynn Fucks -->
+		footSet[30] = "https://file.al/apqcxjmlaz6w/19.m4v.html"; <!-- Toilet Onanism Captured On Hidden Camera -->
+		footSet[31] = "https://file.al/f800pegdwxdb/a.m4v.html"; <!-- Manila Exposed 7 -->
+		footSet[32] = "https://file.al/qju8tw6rnznz/b.m4v.html"; <!-- Manila Exposed 11 -->
+
+var footSetTITLE = new Array();
+		footSetTITLE[1] = "Nica & Sally - Lesbian Lovers (Threesome)";
+		footSetTITLE[2] = "Nica";
+		footSetTITLE[3] = "Pinay Couple Banging While Parents Are Out";
+		footSetTITLE[4] = "Party with Nica";
+		footSetTITLE[5] = "Cartoon from Bangkok Thailand Fucked in the Tub";
+		footSetTITLE[6] = "Fucking Noot from Thailand";
+		footSetTITLE[7] = "Hot Pinay Escorts Agnes & Sunshine";
+		footSetTITLE[8] = "Hot Thai Babe Nok Moans with Pleasure";
+		footSetTITLE[9] = "Home";
+		footSetTITLE[10] = "Manila Exposed 4";
+		footSetTITLE[11] = "Pinay Chicks Janica and Janice Kissing Each Other's Pussy";
+		footSetTITLE[12] = "Phon from Bangkok Thailand Fucked in a Hotel";
+		footSetTITLE[13] = "Thai Girl Not with Thai Pimp Danny Koh";
+		footSetTITLE[14] = "Mitch Hot Pinay Amateur";
+		footSetTITLE[15] = "Koreana Masturbating & Sucking on the Beach";
+		footSetTITLE[16] = "Indonesian Bar Girl Donah Fucked POV";
+		footSetTITLE[17] = "Secret Toilet Camera Voyeur Japanese Student Caught Masturbating";
+		footSetTITLE[18] = "Rubbing My Pussy Till I Cum Hugely";
+		footSetTITLE[19] = "Japanese Girl Student's Room Sex Tutorial Voyeur Caught On Wednesday Jan 8th";
+		footSetTITLE[20] = "Dorm Advantage";
+		footSetTITLE[21] = "Pinay Fucked in Dorm";
+		footSetTITLE[22] = "Gottaluv Cam Show";
+		footSetTITLE[23] = "Real Lesbians Eating Pussy";
+		footSetTITLE[24] = "Russian Amateur Swingers";
+		footSetTITLE[25] = "Security Hidden Camera Caught Sex Scandal";
+		footSetTITLE[26] = "Asian Bound Made To Orgasm";
+		footSetTITLE[27] = "Shy Japanese College Girl Spied While Having Sex";
+		footSetTITLE[28] = "Sogo Motel Cubao Scandal";
+		footSetTITLE[29] = "Tianna Lynn Fucks";
+		footSetTITLE[30] = "Toilet Onanism Captured On Hidden Camera";
+		footSetTITLE[31] = "Manila Exposed 7";
+		footSetTITLE[32] = "Manila Exposed 11";
+
+var min = 1;
+var max = 32;
+var get_seed = Math.floor(Math.random() * (max - min + 1)) + min;
+
+document.write('<a href='+'"'+'javascript:void(0)'+'"'+' onclick='+'"'+'this.href='+'&#x0027;'+footSet[get_seed]+'&#x0027;'+'"'+' target='+'"'+'_blank'+'"'+'><strong>'+footSetTITLE[get_seed]+'</strong></a>');
+}
